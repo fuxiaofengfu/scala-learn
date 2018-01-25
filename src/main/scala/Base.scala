75327897c5dc7bf370caf4a2322f05701ed5816f
xiaofengfu <fuxiaofeng1107@163.com>
2018-01-25 20:50:38 +0800

    case match

--- src/main/scala/Base.scala
+++ src/main/scala/Base.scala
@@ -1,30 +1,48 @@
 object IfElse {
   def main(args: Array[String]): Unit = {
-    println(matchCase("a"))
-    println(partialMatchDouble(2))
+    ifelse(null)
+  }
+
+  def ifelse(p: Any) = {
+    println(p.asInstanceOf[Int])
+    if (p.isInstanceOf[String]) {
+      println(matchCase(p.asInstanceOf[String]))
+    } else {
+      println(partialMatchDouble(p.asInstanceOf[Int]))
+    }
   }
 
   /**
     * 翩函数
+    *
     * @return
     */
-  def partialMatchDouble:PartialFunction[Int,Int]={
+  def partialMatchDouble: PartialFunction[Int, Int] = {
     case 1 => 2 * 1
     case 2 => 2 * 2
     case _ => 0
   }
 
+  def matches(x: String): String = x match {
+    case "a" => val r = x + "_matched"; println(s"ok->$r"); r
+    case "b" => x + "_matched"
+    case "c" => x + "_matched"
+    case "d" => x + "_matched"
+    case _ => return "nothing matches"
+  }
+
   /**
     * 匹配
+    *
     * @param x
     * @return
     */
   def matchCase(x: String): String = {
     x match {
-      case "a" =>  val r = x + "_matched";println(s"ok->$r");r
-      case "b" =>  x + "_matched"
-      case "c" =>  x + "_matched"
-      case "d" =>  x + "_matched"
+      case "a" => val r = x + "_matched"; println(s"ok->$r"); r
+      case "b" => x + "_matched"
+      case "c" => x + "_matched"
+      case "d" => x + "_matched"
       case _ => return "nothing matches"
     }
   }
