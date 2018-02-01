8eee3ef846daef1aaa50147dba862f309945c1a5
xiaofengfu <fuxiaofeng1107@163.com>
2018-02-01 20:43:25 +0800

    first spark program

---
+++ src/main/scala/com/fxf/spark/WordCount.scala
@@ -0,0 +1,39 @@
+package com.fxf.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+class WordCount {
+
+  def wordCount:Unit={
+    val conf = new SparkConf().setAppName("wordcount").setMaster("local")
+    val sc = new SparkContext(conf)
+    val text: RDD[String] = sc.textFile("/Users/xiaofengfu/Downloads/fuxiaofeng.txt")
+    val flat: RDD[String] = text.flatMap(_.split(" "))
+    val map1: RDD[(String, Int)] = flat.map(word=>(word,1))
+    val map2: RDD[(String, Iterable[Int])] = map1.groupByKey()
+    val value: RDD[(String, Int)] = map2.mapValues((a:Iterable[Int])=>a.toList.size)
+    val values: RDD[String] = value.map(x=>x._1+"  "+x._2)
+    values.cache()
+    val strings: Array[String] = values.collect()
+    strings.foreach(println(_))
+  }
+
+  def aggFa(a:Int,b:Int):Int = {
+    println(a,b)
+    a + b
+  }
+
+  def ttt(x:Int = 0) : Unit = {
+    println(x)
+  }
+}
+
+object RunTest{
+  def main(args: Array[String]): Unit = {
+    val count: WordCount = new WordCount()
+    count.wordCount
+  }
+}
+
+
